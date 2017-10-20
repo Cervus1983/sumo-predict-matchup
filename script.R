7e7f40f2d2e5e36ea1c29a5ab0fcd02716401962
Mikhail Zhilkin <mikhail@zhilkin.com>
2017-10-20 16:58:19 +0200

    new approach

--- script.R
+++ script.R
@@ -2,148 +2,170 @@ library(keras)
 library(tidyverse)
 
 
+# data set
+day1 <- do.call(
+	rbind,
+	lapply(
+		list.files("results", "^\\d{6}\\.csv$", full.names = TRUE),
+		read_csv
+	)
+) %>% 
+	filter(day == 1) %>% 
+	transmute(
+		basho = as.character(basho),
+		rank1 = sub("HD$", "", rank1),
+		rank2 = sub("HD$", "", rank2)
+	)
+
+
+# rank string -> ordinal integer
 rank2int <- Vectorize(
-  function(rank_string) as.integer(
-    sprintf(
-      "%d%02d%d",
-      rank_string %>% sub("\\d+.*$", "", .) %>% match(c("Y", "O", "S", "K", "M", "J", "Ms", "Sd", "Jd", "Jk")),
-      rank_string %>% gsub("\\D", "", .) %>% as.integer(),
-      rank_string %>% sub("^.+\\d+", "", .) %>% sub("HD$", "", .) %>% match(c("e", "w"))
-    )
-  )
+	function(rank_string) as.integer(
+		sprintf(
+			"%d%02d%d",
+			rank_string %>% 
+				sub("\\d+.*$", "", .) %>% 
+				match(c("Y", "O", "S", "K", "M", "J", "Ms", "Sd", "Jd", "Jk")),
+			rank_string %>% 
+				gsub("\\D", "", .) %>% 
+				as.integer(),
+			rank_string %>% 
+				sub("^.+\\d+", "", .) %>% 
+				sub("HD$", "", .) %>% 
+				match(c("e", "w"))
+		)
+	)
 )
 
 
-results <- do.call(
-  rbind,
-  lapply(
-    list.files("results", "^\\d{6}\\.csv$", full.names = TRUE),
-    read_csv
-  )
-) %>% 
-  # remove play-offs
-  filter(day < 16)
+# all makuuchi ranks found in data set
+makuuchi <- tibble(rank_string = c(day1$rank1, day1$rank2) %>% unique()) %>% 
+	arrange(rank2int(rank_string)) %>% 
+	filter(rank2int(rank_string) < 6000) %>% 
+	pull(rank_string)
 
 
-day1 <- results %>% 
-  filter(day == 1) %>% 
-  transmute(
-    basho,
-    rank1 = sub("HD$", "", rank1),
-    rank2 = sub("HD$", "", rank2)
-  )
-
-day1 <- rbind(
-  day1 %>% filter(rank2int(rank1) < 6000) %>% select(basho, rank.x = rank1, rank.y = rank2),
-  day1 %>% filter(rank2int(rank2) < 6000) %>% select(basho, rank.x = rank2, rank.y = rank1)
-) %>% 
-  arrange(basho, rank2int(rank.x)) %>% 
-  mutate(rank.y = ifelse(rank2int(rank.y) < 6000, rank.y, "J"))
-
-
-ranks <- day1 %>% 
-  select(rank.y) %>% 
-  unique() %>% 
-  arrange(rank2int(rank.y)) %>% 
-  pull(rank.y)
-
-
-rank2categorical <- Vectorize(
-  function(rank_string) rank_string %>% 
-    match(., ranks) %>% 
-    `-`(1) %>% 
-    to_categorical(num_classes = length(ranks))
+# encodes & decodes rank strings
+one_hot <- function(ranks) as.integer(
+	1:(length(makuuchi) + 1) %in% match(ranks, makuuchi, nomatch = length(makuuchi) + 1)
 )
 
+rev_one_hot <- function(m) apply(m, 1, which.max) %>% makuuchi[.]
 
-banzuke <- sapply(
-  day1 %>% pull(basho) %>% unique(),
-  function(b) day1 %>% 
-    filter(basho == b) %>% 
-    pull(rank.x) %>% 
-    rank2categorical() %>% 
-    apply(1, max)
+
+# arranges matchups as high rank vs low rank & sorts by high rank
+banzuke_sort <- function(banzuke) banzuke %>% 
+	transmute(
+		rank_high = ifelse(rank2int(rank1) < rank2int(rank2), rank1, rank2),
+		rank_low = ifelse(rank2int(rank1) > rank2int(rank2), rank1, rank2)
+	) %>% 
+	arrange(rank2int(rank_high))
+
+
+# basho (yyyy.mm) -> matrix with 0's and 1's
+basho2xy <- function(b) {
+	tmp <- day1 %>% 
+		filter(basho == b) %>% 
+		banzuke_sort()
+	
+	do.call(
+		rbind,
+		lapply(
+			1:nrow(tmp),
+			function(i) c(
+				tmp %>% 
+					filter(row_number() >= i) %>% 
+					gather(var, val) %>% 
+					pull(val) %>% 
+					tail(-1) %>% 
+					one_hot(),
+				tmp[i, 2] %>% one_hot()
+			)
+		)
+	)
+}
+
+
+# process & split data set
+xy <- do.call(
+	rbind,
+	lapply(
+		day1 %>% pull(basho) %>% unique(),
+		basho2xy
+	)
 )
 
+x_train <- xy[-(3184:3204), 1:(length(makuuchi) + 1)]
+y_train <- xy[-(3184:3204), (length(makuuchi) + 2):((length(makuuchi) + 1) * 2)]
 
-x <- do.call(
-  rbind,
-  lapply(
-    1:nrow(day1),
-    function(i) c(
-      day1[i, ]$rank.x %>% rank2categorical(),
-      day1 %>% pull(basho) %>% unique() %>% match(day1[i, ]$basho, .) %>% banzuke[, .]
-    )
-  )
-)
+x_test <- xy[3184:3204, 1:(length(makuuchi) + 1)]
+y_test <- xy[3184:3204, (length(makuuchi) + 2):((length(makuuchi) + 1) * 2)]
 
 
-y <- do.call(
-  rbind,
-  lapply(
-    1:nrow(day1),
-    function(i) day1[i, ]$rank.y %>% rank2categorical() %>% t()
-  )
-)
-
-
+# model
 model <- keras_model_sequential() %>% 
 	layer_dense(
-		units = dim(x)[2] * 20,
+		units = (length(makuuchi) + 1) * 64,
 		activation = "relu",
-		input_shape = dim(x)[2]
+		input_shape = length(makuuchi) + 1
 	) %>% 
+	#layer_dropout(rate = .25) %>% 
 	layer_dense(
-		units = dim(x)[2] * 10,
+		units = (length(makuuchi) + 1) * 16,
 		activation = "relu",
 	) %>% 
-  #layer_dropout(rate = .25) %>% 
+	#layer_dropout(rate = .25) %>% 
 	layer_dense(
-		units = dim(x)[2] * 5,
+		units = (length(makuuchi) + 1) * 4,
 		activation = "relu"
 	) %>% 
-  #layer_dropout(rate = .5) %>% 
-  layer_dense(
-		units = dim(y)[2],
+	#layer_dropout(rate = .25) %>% 
+	layer_dense(
+		units = length(makuuchi) + 1,
 		activation = "softmax"
 	)
 
-model %>% 
-  compile(
-  	optimizer = "adam",
-  	loss = "categorical_crossentropy",
-  	metrics = "accuracy"
-  )
+#my_loss <- function(y_true, y_pred) 1 - sum(y_true * y_pred)
 
 model %>% 
-  fit(
-  	head(x, -37),
-  	head(y, -37),
-  	epochs = 10,
-  	callbacks = callback_tensorboard(),
-  	validation_split = .2
-  )
+	compile(
+		optimizer = optimizer_adam(lr = 0.0001),
+		loss = "categorical_crossentropy",# my_loss,
+		metrics = "accuracy"
+	)
 
-model %>% evaluate(tail(x, 37), tail(y, 37))
-
-
-# duplicate rank.y.pred
-day1_pred <- day1 %>% 
-  tail(37) %>% 
-  mutate(rank.y.pred = NA)
-
-m <- model %>% predict(tail(x, 37))
-
-m <- t(t(m) * c(head(banzuke[, 160], -1), 1))
+model %>% 
+	fit(
+		x_train,
+		y_train,
+		epochs = 10,
+		callbacks = callback_tensorboard(),
+		validation_split = .2
+	)
 
 
-while (day1_pred %>% filter(!complete.cases(.)) %>% nrow() > 0) {
-  max_probability <- which(m == max(m), arr.ind = TRUE)
-  
-  if (is.na(day1_pred[max_probability[1], ]$rank.y.pred)) {
-    day1_pred[max_probability[1], ]$rank.y.pred <- ranks[max_probability[2]]
-    day1_pred[day1_pred$rank.x == ranks[max_probability[2]], ]$rank.y.pred <- day1_pred[max_probability[1], ]$rank.x
-  }
-  
-  m[max_probability[1], max_probability[2]] <- 0
+# moment of truth
+result <- "Y1e"
+
+x <- x_test[1, ]
+
+while (sum(x) > 0) {
+	matchup <- x %>% 
+		t() %>% 
+		predict(model, .) %>% 
+		which.max() %>% 
+		makuuchi[.]
+	
+	up_next <- x %>% 
+		which.max() %>% 
+		makuuchi[.]
+	
+	x <- x * (1 - one_hot(matchup)) * (1 - one_hot(up_next))
+	
+	result <- c(result, matchup, up_next)
 }
+
+result %>% 
+	matrix(ncol = 2, byrow = TRUE) %>% 
+	as.tibble() %>% 
+	View()
